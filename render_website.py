4d824238270076371d1081a3c3c75948c2c8b69b
Ангелина Петрова <linalleks@gmail.com>
2026-05-12 16:57:27 +0700

    Prepare site to work offline

--- render_website.py
+++ render_website.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     on_reload()
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='./pages', default_filename='index1.html')
+    server.serve(root='.', default_filename='./pages/index1.html')
