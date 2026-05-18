e2351b142800f8bcef28636f06c7bd604f0fc768
Ангелина Петрова <linalleks@gmail.com>
2026-05-18 19:27:14 +0700

    added option to set json-file path

--- render_website.py
+++ render_website.py
@@ -2,16 +2,34 @@ import json
 from more_itertools import chunked
 from pathlib import Path
 
+from dotenv import load_dotenv
+import click
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+import shutil
+
+load_dotenv()
 
 
-def on_reload():
-    with open('meta_data.json', 'r', encoding='utf-8') as file:
+@click.command(help='JSON_PATH: Путь к json-файлу с данными по книгам онлайн-библиотеки')
+@click.argument('json_path', envvar='JSON_PATH', default='meta_data.json', type=click.Path(exists=True))
+def main(json_path):
+    on_reload(json_path)
+    server = Server()
+    server.watch('template.html', on_reload(json_path))
+    server.serve(root='.', default_filename='./pages/index1.html')
+
+
+def on_reload(json_path):
+    with open(json_path, 'r', encoding='utf-8') as file:
         books = json.load(file)
 
     pages = list(chunked(books, 20))
     count_pages = len(pages)
+
+    pages_dir = Path("pages")
+    if pages_dir.exists() and pages_dir.is_dir():
+        shutil.rmtree(pages_dir)
 
     for num, books_page in enumerate(pages, 1):
         env = Environment(
@@ -34,7 +52,4 @@ def on_reload():
 
 
 if __name__ == '__main__':
-    on_reload()
-    server = Server()
-    server.watch('template.html', on_reload)
-    server.serve(root='.', default_filename='./pages/index1.html')
+    main()
